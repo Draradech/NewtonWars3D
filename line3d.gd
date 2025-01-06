daa1b846ebe219609830cf078431ef7461021bf8
Manuel Kasten <kasten.m@gmx.de>
2025-01-06 18:28:03 +0100

    line finalize and line temp end (for interpolation)

--- line3d.gd
+++ line3d.gd
@@ -1,30 +1,164 @@
 class_name Line3D
 extends Node3D
 
-const sides = 8
-const radius = .02
-const surface_limit: int = 1000 / sides
+const sides: int = 8
+const radius: float = .02
+const seg_limit: int = 1000 / sides
 
 var material: BaseMaterial3D
+var points: PackedVector3Array
+
+var mesh: MeshInstance3D
 var surface: Array
 var verts: PackedVector3Array
 var indices: PackedInt32Array
-var points: PackedVector3Array
-var mesh: MeshInstance3D
+
+var tmpmesh: MeshInstance3D
+var tmpsurface: Array
+var tmpverts: PackedVector3Array
+var tmpindices: PackedInt32Array
+
+# todo needs cleanup, unneccessary duplicated code
+func tmpEnd(p: Vector3):
+	var dir: Vector3
+	var right: Vector3
+	var vert0: Vector3
+	
+	tmpverts.clear()
+	tmpindices.clear()
+	if not tmpmesh:
+		tmpmesh = MeshInstance3D.new()
+		tmpmesh.mesh = ArrayMesh.new()
+		add_child(tmpmesh)
+	
+	if points.size() > 2:
+		# copy end vertices of last drawn segment
+		for i in range(sides):
+			tmpverts.append(verts[-sides + i])
+		
+		# calc verts at last known point in line
+		dir = (p - points[-2]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-1])
+		
+		# calc verts at temp point
+		dir = (p - points[-1]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + p)
+		
+		# tube
+		for j in range(2):
+			for i in range(sides + 1):
+				tmpindices.append(j * sides +       + (0 + i) % sides)
+				tmpindices.append(j * sides + sides + (0 + i) % sides)
+				tmpindices.append(j * sides +       + (1 + i) % sides)
+				tmpindices.append(j * sides +       + (1 + i) % sides)
+				tmpindices.append(j * sides + sides + (0 + i) % sides)
+				tmpindices.append(j * sides + sides + (1 + i) % sides)
+		
+		# cap end point
+		for i in range(sides - 2):
+			tmpindices.append(sides + sides + 0)
+			tmpindices.append(sides + sides + i + 2)
+			tmpindices.append(sides + sides + i + 1)
+		
+	elif points.size() == 2:
+		# calc verts at point 0
+		dir = (points[-1] - points[-2]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-2])
+		
+		# calc verts at point 1
+		dir = (p - points[-2]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-1])
+		
+		# calc verts at temp point
+		dir = (p - points[-1]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + p)
+		
+		# tube
+		for j in range(2):
+			for i in range(sides + 1):
+				tmpindices.append(j * sides +       + (0 + i) % sides)
+				tmpindices.append(j * sides + sides + (0 + i) % sides)
+				tmpindices.append(j * sides +       + (1 + i) % sides)
+				tmpindices.append(j * sides +       + (1 + i) % sides)
+				tmpindices.append(j * sides + sides + (0 + i) % sides)
+				tmpindices.append(j * sides + sides + (1 + i) % sides)
+		
+		# cap start point
+		for i in range(sides - 2):
+			tmpindices.append(0)
+			tmpindices.append(i + 1)
+			tmpindices.append(i + 2)
+		
+		# cap end point
+		for i in range(sides - 2):
+			tmpindices.append(sides + sides + 0)
+			tmpindices.append(sides + sides + i + 2)
+			tmpindices.append(sides + sides + i + 1)
+	elif points.size() == 1:
+		# calc verts at point 0
+		dir = (p - points[-1]).normalized()
+		right = dir.cross(Vector3.UP).normalized()
+		vert0 = right * radius
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-1])
+		# calc verts at temp point
+		for i in range(sides):
+			tmpverts.append(vert0.rotated(dir, i * 2 * PI / sides) + p)
+		
+		# tube
+		for i in range(sides + 1):
+			tmpindices.append(      + (0 + i) % sides)
+			tmpindices.append(sides + (0 + i) % sides)
+			tmpindices.append(      + (1 + i) % sides)
+			tmpindices.append(      + (1 + i) % sides)
+			tmpindices.append(sides + (0 + i) % sides)
+			tmpindices.append(sides + (1 + i) % sides)
+		
+		# cap start point
+		for i in range(sides - 2):
+			tmpindices.append(0)
+			tmpindices.append(i + 1)
+			tmpindices.append(i + 2)
+		
+		# cap end point
+		for i in range(sides - 2):
+			tmpindices.append(sides + 0)
+			tmpindices.append(sides + i + 2)
+			tmpindices.append(sides + i + 1)
+	
+	tmpmesh.mesh.clear_surfaces()
+	tmpmesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, tmpsurface)
+	tmpmesh.mesh.surface_set_material(0, material)
+
 
 func addPoint(p: Vector3):
 	points.push_back(p)
-	var point_num = points.size() - 1
+	var point_num: int = points.size() - 1
 	
 	if point_num < 2: return
 	
-	var surface_num = (point_num - 2) / surface_limit
-	var point_in_s = (point_num - 2) % surface_limit
-	var dir
-	var r
-	var v0
+	var surface_num: int = (point_num - 2) / seg_limit
+	var seg_in_surf: int = (point_num - 2) % seg_limit
+	var dir: Vector3
+	var right: Vector3
+	var vert0: Vector3
 	
-	if point_in_s == 0:
+	if seg_in_surf == 0:
 		var oldverts = verts
 		verts = PackedVector3Array()
 		surface[Mesh.ARRAY_VERTEX] = verts
@@ -34,27 +168,92 @@ func addPoint(p: Vector3):
 		add_child(mesh)
 		if surface_num == 0:
 			dir = (points[-2] - points[-3]).normalized()
-			r = dir.cross(Vector3.UP).normalized()
-			v0 = r * radius
+			right = dir.cross(Vector3.UP).normalized()
+			vert0 = right * radius
 			for i in range(sides):
-				verts.append(v0.rotated(dir, i * 2 * PI / sides) + points[-3])
+				verts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-3])
 		else:
 			for i in range(sides):
 				verts.append(oldverts[-sides + i])
 	
 	dir = (points[-1] - points[-3]).normalized()
-	r = dir.cross(Vector3.UP).normalized()
-	v0 = r * radius
+	right = dir.cross(Vector3.UP).normalized()
+	vert0 = right * radius
 	for i in range(sides):
-		verts.append(v0.rotated(dir, i * 2 * PI / sides) + points[-2])
+		verts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-2])
+	
+	# cap start point
+	if point_num == 2:
+		for i in range(sides - 2):
+			indices.append(0)
+			indices.append(i + 1)
+			indices.append(i + 2)
 	
 	for i in range(sides + 1):
-		indices.append(point_in_s * sides         + (0 + i) % sides)
-		indices.append(point_in_s * sides + sides + (0 + i) % sides)
-		indices.append(point_in_s * sides         + (1 + i) % sides)
-		indices.append(point_in_s * sides         + (1 + i) % sides)
-		indices.append(point_in_s * sides + sides + (0 + i) % sides)
-		indices.append(point_in_s * sides + sides + (1 + i) % sides)
+		indices.append(seg_in_surf * sides         + (0 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (0 + i) % sides)
+		indices.append(seg_in_surf * sides         + (1 + i) % sides)
+		indices.append(seg_in_surf * sides         + (1 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (0 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (1 + i) % sides)
+	
+	mesh.mesh.clear_surfaces()
+	mesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface)
+	mesh.mesh.surface_set_material(0, material)
+
+func finalize():
+	var point_num: int = points.size() # pretend we added a new virtual point
+	
+	if point_num < 2: return
+	
+	var surface_num: int = (point_num - 2) / seg_limit
+	var seg_in_surf: int = (point_num - 2) % seg_limit
+	var dir: Vector3
+	var right: Vector3
+	var vert0: Vector3
+	
+	dir = (points[-1] - points[-2]).normalized()
+	right = dir.cross(Vector3.UP).normalized()
+	vert0 = right * radius
+	
+	if seg_in_surf == 0:
+		var oldverts = verts
+		verts = PackedVector3Array()
+		surface[Mesh.ARRAY_VERTEX] = verts
+		indices.clear()
+		mesh = MeshInstance3D.new()
+		mesh.mesh = ArrayMesh.new()
+		add_child(mesh)
+		if surface_num == 0:
+			for i in range(sides):
+				verts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-2])
+		else:
+			for i in range(sides):
+				verts.append(oldverts[-sides + i])
+	
+	for i in range(sides):
+		verts.append(vert0.rotated(dir, i * 2 * PI / sides) + points[-1])
+	
+	# cap start point
+	if point_num == 2:
+		for i in range(sides - 2):
+			indices.append(0)
+			indices.append(i + 1)
+			indices.append(i + 2)
+	
+	for i in range(sides + 1):
+		indices.append(seg_in_surf * sides         + (0 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (0 + i) % sides)
+		indices.append(seg_in_surf * sides         + (1 + i) % sides)
+		indices.append(seg_in_surf * sides         + (1 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (0 + i) % sides)
+		indices.append(seg_in_surf * sides + sides + (1 + i) % sides)
+	
+	# cap end point
+	for i in range(sides - 2):
+		indices.append(seg_in_surf * sides + sides + 0)
+		indices.append(seg_in_surf * sides + sides + i + 2)
+		indices.append(seg_in_surf * sides + sides + i + 1)
 	
 	mesh.mesh.clear_surfaces()
 	mesh.mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, surface)
@@ -64,3 +263,6 @@ func _init():
 	surface.resize(Mesh.ARRAY_MAX)
 	surface[Mesh.ARRAY_VERTEX] = verts
 	surface[Mesh.ARRAY_INDEX] = indices
+	tmpsurface.resize(Mesh.ARRAY_MAX)
+	tmpsurface[Mesh.ARRAY_VERTEX] = tmpverts
+	tmpsurface[Mesh.ARRAY_INDEX] = tmpindices
--- spiral.gd
+++ spiral.gd
@@ -2,16 +2,55 @@
 extends Node3D
 
 @export var material: BaseMaterial3D
-var t = 0.0
+
+var tpnext: float = 0.0
+var ppnext: Vector3 = Vector3(0, sin(0), cos(0))
+var tplast: float = 0.0
+var pplast: Vector3 = Vector3(0, sin(0), cos(0))
+var rplast: Vector3 = Vector3(0, sin(0), cos(0))
+var rpnext: Vector3 = Vector3(0, sin(0), cos(0))
+var rtpnext: float = 0
+var rtplast: float = 0
+
+var tr: float = 0.0
+
 var line: Line3D
+var line2: Line3D
 
 func _ready():
 	line = Line3D.new()
 	line.material = material
+	line2 = Line3D.new()
+	line2.material = material
 	add_child(line)
+	add_child(line2)
+	
+	line2.addPoint(Vector3(0,0,0))
+	line2.addPoint(Vector3(.5,.5,0))
+	#line2.addPoint(Vector3(1.,.5,.5))
+	line2.finalize()
 
+var stop = 0
+func _process(delta: float):
+	if tr + delta > tpnext:
+		return
+	if tr <= tplast and tr + delta > tplast:
+		line.addPoint(pplast)
+		rplast = pplast
+		rpnext = ppnext
+		rtplast = tplast
+		rtpnext = tpnext
+	tr += delta
+	line.tmpEnd(rplast + (rpnext - rplast) * (tr - rtplast) / (rtpnext - rtplast))
+
+var i: int = 0
+var slowdown: int = 10
 func _physics_process(delta: float):
-	if t > 100: return
-	for i in range(10):
-		t += delta
-		line.addPoint(Vector3(.5 * t, sin(.5 * t * 2 * PI), cos(.5 * t * 2 * PI)))
+	i += 1
+	if i % slowdown == 0:
+		pplast = ppnext
+		tplast = tpnext
+		tpnext += delta * slowdown
+		var tm = tpnext / slowdown
+		var l = 1.
+		ppnext = Vector3(l * tm, sin(l * tm * 2 * PI), cos(l * tm * 2 * PI))
