8b2283782a653ce96beeac1156bb068da95d440a
Manuel Kasten <kasten.m@gmx.de>
2025-01-16 19:36:10 +0100

    match server version

--- client/addons/git_version/git_version.gd
+++ client/addons/git_version/git_version.gd
@@ -9,36 +9,29 @@ func _enter_tree() -> void:
 func _exit_tree() -> void:
 	remove_export_plugin(exporter)
 
+func _build() -> bool:
+	exporter.create_version_script()
+	return true
+
 class GitVersionExporterPlugin extends EditorExportPlugin:
 	func _get_name() -> String:
 		return "GitVersionExporterPlugin"
 	
-	func get_git_commit_count() -> String:
+	func get_git_description() -> String:
 		var output: Array = []
-		OS.execute("git", PackedStringArray(["rev-list", "--count", "HEAD"]), output)
+		OS.execute("git", PackedStringArray(["describe", "--tags", "--dirty", "--match", "v*"]), output)
 		if output.is_empty() or output[0].is_empty():
 			push_error("Failed to fetch version. Make sure you have git installed and project is inside a valid git directory.")
-			return ""
+			return "v0.0.0-unknown"
 		return output[0].trim_suffix("\n")
 	
-	func get_git_commit_hash() -> String:
-		var output: Array = []
-		OS.execute("git", PackedStringArray(["rev-parse", "--short=8", "HEAD"]), output)
-		if output.is_empty() or output[0].is_empty():
-			push_error("Failed to fetch version. Make sure you have git installed and project is inside a valid git directory.")
-			return ""
-		return output[0].trim_suffix("\n")
-	
-	func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
-		var version: = "0.1."
-		version += get_git_commit_count()
-		ProjectSettings.set_setting("application/config/version", version)
-		ProjectSettings.save()
-		version += "-"
-		version += get_git_commit_hash()
-		print(version)
+	func create_version_script():
+		var version: = get_git_description()
 		var script: GDScript = GDScript.new()
 		script.source_code = "extends Node\nconst version: String = \"%s\"" % version
 		var err: int = ResourceSaver.save(script, "res://scripts/version.gd")
 		if err:
 			push_error("Failed to save version as script. Error: %s" % error_string(err))
+	
+	func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
+		create_version_script()
--- client/scripts/version.gd
+++
@@ -1,2 +0,0 @@
-extends Node
-const version: String = "0.1.38-dd5b25d6"
